41e5f469eb80a563564f1d3c2be77472a6840cd1
Andrzej Westfalewicz <awestfal@gmail.com>
2018-09-27 00:38:04 +0200

    Added silent logger

---
+++ FileDeduplicator/Logging/SilentLogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileDeduplicator.Logging
+{
+    class SilentLogger : ILogger
+    {
+        public void Dispose() { }
+
+        public void WriteLine(string text) { }
+
+        public void WriteLine() { }
+    }
+}
