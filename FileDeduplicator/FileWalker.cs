7dbe97aa09ed50b880816ebb816e1923ae12f8b7
Andrzej Westfalewicz <awestfal@gmail.com>
2018-09-27 22:56:04 +0200

    Percentage summary added

--- FileDeduplicator/FileWalker.cs
+++ FileDeduplicator/FileWalker.cs
@@ -10,6 +10,7 @@ namespace FileDeduplicator
     class FileWalker
     {
         public Dictionary<string, List<string>> fileToPath = new Dictionary<string, List<string>>();
+        public Dictionary<string, int> filesCountPerFolder = new Dictionary<string, int>();
 
         ILogger _logger;
         string _path;
@@ -46,22 +47,29 @@ namespace FileDeduplicator
         private void AddFile(string file)
         {
             string fileName = Path.GetFileName(file);
+            string folderName = Path.GetDirectoryName(file);
             _logger.WriteLine(fileName);
 
             if (fileToPath.ContainsKey(fileName))
             {
-                fileToPath[fileName].Add(file);
+                fileToPath[fileName].Add(folderName);
             }
             else
             {
                 fileToPath[fileName] = new List<string>();
-                fileToPath[fileName].Add(file);
+                fileToPath[fileName].Add(folderName);
             }
+
+            if (filesCountPerFolder.ContainsKey(folderName))
+                filesCountPerFolder[folderName]++;
+            else
+                filesCountPerFolder[folderName] = 1;
         }
 
 
         public void ProcessResult()
         {
+            var duplicatesPerFolder = new Dictionary<string, int>();
             _logger.WriteLine();
             _logger.WriteLine("SUMMARY");
             _logger.WriteLine();
@@ -70,17 +78,28 @@ namespace FileDeduplicator
             foreach (var item in fileToPath.Where(kv => kv.Value.Count > 2).Where(kv=> !FileWalkerConfig.ignoredFileNames.Contains(kv.Key)))
             {
                 dubbedKeys++;
-                Console.WriteLine($"FILE: {item.Key} is in multiple folders:");
+                _logger.WriteLine($"FILE: {item.Key} is in multiple folders:");
                 foreach (var folder in item.Value)
                 {
                     dubbedValues++;
                     _logger.WriteLine(folder);
+                    if (duplicatesPerFolder.ContainsKey(folder))
+                        duplicatesPerFolder[folder]++;
+                    else
+                        duplicatesPerFolder[folder] = 1;
                 }
             }
             _logger.WriteLine();
-            _logger.WriteLine("SUMMARY of SUMMARY");
             _logger.WriteLine();
             _logger.WriteLine($"You can remove {dubbedValues-dubbedKeys} files");
+
+
+            _logger.WriteLine($"Directories with duplicates ({duplicatesPerFolder.Keys.Count}):");
+            foreach (var folder in duplicatesPerFolder.Keys.OrderBy(f=>f))
+            {
+                float percentage = (float)duplicatesPerFolder[folder] / filesCountPerFolder[folder];
+                _logger.WriteLine($"{folder} is in {percentage.ToString("p")} duplicated");
+            }
         }
     }
 }
